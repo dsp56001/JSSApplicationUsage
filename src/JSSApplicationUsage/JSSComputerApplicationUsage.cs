f454e27aa83236e40107a12a38f36039dad0352a
Jeff Meyers <jmeyers@colum.edu>
2016-10-14 09:06:54 -0500

    Inintal Commit

---
+++ src/JSSApplicationUsage/JSSComputerApplicationUsage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSSUsage.Computer_Application_Usage
+{
+    
+
+        public class Rootobject
+        {
+            public Computer_Application_Usage[] computer_application_usage { get; set; }
+        }
+
+        public class Computer_Application_Usage
+        {
+            public string date { get; set; }
+            public App[] apps { get; set; }
+        }
+
+        public class App
+        {
+            public string name { get; set; }
+            public string version { get; set; }
+            public int foreground { get; set; }
+            public int open { get; set; }
+        }
+
+    
+}
